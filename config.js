561c44c5a6d348980d52018ea9799be79f864f39
supernova789 <50611789+supernova789@users.noreply.github.com>
2020-09-02 15:08:37 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
     items: {
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3000
     },
     info: {
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 4200
     }
    };
